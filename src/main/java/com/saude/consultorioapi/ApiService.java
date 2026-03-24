8e6a2ce2a299fd584050141847848d49b448c570
Victor Hugo <hugoowvicttor@gmail.com>
2026-03-24 01:47:16 -0300

    .

--- src/main/java/com/saude/consultorioapi/ApiService.java
+++ src/main/java/com/saude/consultorioapi/ApiService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import javax.net.ssl.*;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -16,7 +17,7 @@ public class ApiService {
 
     private final String URL = "https://apichallenges.eviltester.com";
 
-    public ApiResponse httpRequest(String urlString, String method) throws IOException {
+    public ApiResponse httpRequest(String urlString, String method, String payload, String contentType) throws IOException {
         disableSSLverification();
         String entitiesURL = URL+urlString;
 
@@ -25,6 +26,20 @@ public class ApiService {
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
         connection.setRequestMethod(method);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        if(payload != null && ("Post".equals(method)) || ("PUT".equals(method))) {
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", contentType != null ? contentType : "application/json;charset=utf-8");
+
+            try(DataOutputStream dos = new DataOutputStream(connection.getOutputStream())){
+                byte[] sendData = payload.getBytes(StandardCharsets.UTF_8);
+
+                dos.write(sendData);
+                dos.flush();
+            }
+        }
 
         int responseCode = connection.getResponseCode();
 
--- src/main/java/com/saude/consultorioapi/ConsultorioapiApplication.java
+++ src/main/java/com/saude/consultorioapi/ConsultorioapiApplication.java
@@ -29,6 +29,26 @@ public class ConsultorioapiApplication {
         obterPorCategoriaELimite("teste",5);
         //Questão 5
         System.out.println("Questão 5:");
+        criarEntidade("aluno");
+        //Questão 6
+        System.out.println("Questão 6:");
+        obterEntidadeEspecifica(11);
+        //Questão 7
+        System.out.println("Questão 7:");
+        atualizarEntidade("atualizado",10);
+        //Questão 8
+        System.out.println("Questão 8:");
+        alterarEntidade("atualizado",10);
+        //Questão 9
+        System.out.println("Questão 9:");
+        excluirEntidade(9);
+        obterEntidadeEspecifica(9);
+        //Questão 10
+        System.out.println("Questão 10:");
+        excluirEntidade(2);
+        //Questão 11
+        System.out.println("Questão 11:");
+        obterOptions();
 
 
 
@@ -37,7 +57,7 @@ public class ConsultorioapiApplication {
     private static void obterLista() throws IOException{
         apiService = new ApiService();
 
-        ApiResponse response = apiService.httpRequest("/sim/entities", "GET");
+        ApiResponse response = apiService.httpRequest("/sim/entities", "GET", null, null);
 
         System.out.println(response);
 
@@ -47,7 +67,7 @@ public class ConsultorioapiApplication {
     private static void obterEntidadeEspecifica(int id) throws IOException{
         apiService = new ApiService();
 
-        ApiResponse response = apiService.httpRequest("/sim/entities/"+id, "GET");
+        ApiResponse response = apiService.httpRequest("/sim/entities/"+id, "GET", null, null);
 
         System.out.println(response);
 
@@ -57,19 +77,63 @@ public class ConsultorioapiApplication {
     private static void obterPorCategoriaELimite(String categoria, int limite) throws IOException{
         apiService = new ApiService();
 
-        ApiResponse response = apiService.httpRequest("/sim/entities?categoria="+categoria+"&limite="+limite, "GET");
+        ApiResponse response = apiService.httpRequest("/sim/entities?categoria="+categoria+"&limite="+limite, "GET", null, null);
 
         System.out.println(response);
 
     }
 
     // POST criando uma nova entidade
-    private static void criarEntidade() throws IOException{
+    private static void criarEntidade(String nome) throws IOException{
         apiService = new ApiService();
 
-        ApiResponse response = apiService.httpRequest("", "POST");
+        String payload = String.format("{\";name\": \"%s\"}", nome);
+
+        ApiResponse response = apiService.httpRequest("/sim/entities", "POST", payload, null);
 
         System.out.println(response);
 
+    }
+
+    //POST para atualizar uma entidade
+    private static void atualizarEntidade(String nome, int id) throws IOException{
+        apiService = new ApiService();
+
+        String payload = String.format("{\";name\": \"%s\"}", nome);
+
+        ApiResponse response = apiService.httpRequest("/sim/entities/"+id, "POST", payload, null);
+
+        System.out.println(response);
+
+    }
+
+    //PUT para atualizar entidade
+    private static void alterarEntidade(String nome, int id) throws IOException{
+        apiService = new ApiService();
+
+        String payload = String.format("{\";name\": \"%s\"}", nome);
+
+        ApiResponse response = apiService.httpRequest("/sim/entities/"+id, "PUT", payload, null);
+
+        System.out.println(response);
+
+    }
+
+    // DELETE de entidade válida
+    private static void excluirEntidade(int id) throws IOException{
+        apiService = new ApiService();
+
+        ApiResponse response = apiService.httpRequest("/sim/entities/"+id, "DELETE", null, null);
+
+        System.out.println(response);
+    }
+
+    // OPTIONS com verificação de métodos
+    private static void obterOptions() throws IOException{
+        apiService = new ApiService();
+
+        ApiResponse response = apiService.httpRequest("/sim/entities/", "OPTIONS", null, null);
+
+        System.out.println(response);
     }
 }
